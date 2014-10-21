6c7cff980d9e2dfa212f115c6019690cec8f2cb0
OokamiNya <42chrono@gmail.com>
2014-10-21 12:25:26 -0400

    added check for posts with same title, /post/title stuff

--- yolo.py
+++ yolo.py
@@ -2,12 +2,12 @@ from flask import Flask,render_template,request
 import sqlite3
 
 app = Flask(__name__) 
-
+postdb = "yolo.db"
 ###
 
 @app.route("/test",methods=['GET','POST'])
 def home():
-    conn = sqlite3.connect("yolo.db")
+    conn = sqlite3.connect(postdb)
     curs = conn.cursor()
     if request.method=="POST":
         query = "SELECT count(*) FROM posts"
@@ -15,6 +15,11 @@ def home():
         newid = result.fetchone()[0] + 1
         newtitle = request.form["title"]
         newbody = request.form["body"]
+        query = "SELECT * FROM posts WHERE title=?"
+        curs.execute(query,[newtitle])
+        if len(curs.fetchall())>0:
+            return render_template("error.html",
+                                   title=newtitle)
         query = "INSERT INTO posts VALUES (?,?,?)"
         curs.execute(query,[newid,newtitle,newbody])
         conn.commit()
@@ -24,6 +29,16 @@ def home():
     print posts
     return render_template("test.html",
                            posts=posts)
+
+@app.route("/post/<title>")
+def getpost(title):
+    conn = sqlite3.connect(postdb)
+    curs = conn.cursor()
+    query = "SELECT * FROM posts WHERE title=?"
+    curs.execute(query,[title])
+    post = curs.fetchone()
+    return render_template("post.html",
+                           post=post)
 
 @app.route("/newpost",methods=['GET','POST'])
 def newpost():
